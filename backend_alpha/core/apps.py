b6f67072fe9b75971d7c2752446ccded3873655d
Graham Whitehouse <graham.whitehouse@ukhsa.gov.uk>
2025-01-13 12:26:36 +0000

    Updated all formatting issues

--- backend_alpha/core/apps.py
+++ backend_alpha/core/apps.py
@@ -7,4 +7,5 @@ class CoreConfig(AppConfig):
     def ready(self):
         from core.orders import signals
         from core.products import signals
+
         # Import signal handlers here to ensure they're registered

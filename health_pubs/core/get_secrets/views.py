cd91cc45b0cdc1dccc868118cd45d3b731a13eb3
ebubeoguchi34 <ebube.oguchi@ukhsa.gov.uk>
2025-01-28 09:57:34 +0000

    Update health_pubs/core/get_secrets/views.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- health_pubs/core/get_secrets/views.py
+++ health_pubs/core/get_secrets/views.py
@@ -1,5 +1,9 @@
 import json
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+    authentication_classes,
+)
 from rest_framework.response import Response
 
 from rest_framework.permissions import AllowAny

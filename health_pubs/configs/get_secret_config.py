0cfad1febfe6853f140d8074479c1876879efed2
ebubeoguchi34 <ebube.oguchi@ukhsa.gov.uk>
2025-02-10 17:16:48 +0000

    Update health_pubs/configs/get_secret_config.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- health_pubs/configs/get_secret_config.py
+++ health_pubs/configs/get_secret_config.py
@@ -191,7 +191,9 @@ class Config:
 
     @staticmethod
     def get_hpub_event_bridge_detail_type_order_creation():
-        return Config.get_value("HPUB_EVENT_BRIDGE_DETAIL_TYPE_ORDER_CREATION", is_secret=True)
+        return Config.get_value(
+            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_ORDER_CREATION", is_secret=True
+        )
 
     @staticmethod
     def get_hpub_event_bridge_detail_type_product_draft():

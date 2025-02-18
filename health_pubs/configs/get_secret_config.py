d6ebc0a50e326769f5f5f609fbf9900a619d0182
ebubeoguchi34 <ebube.oguchi@ukhsa.gov.uk>
2025-02-18 13:54:11 +0000

    refactor: streamline database configuration retrieval and simplify environment loading

--- health_pubs/configs/get_secret_config.py
+++ health_pubs/configs/get_secret_config.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import os
-import re
 import sys
 from typing import Optional
 
@@ -88,28 +87,28 @@ class Config:
     @staticmethod
     def get_aps_api_key():
         """Retrieve the APS API key from secrets manager."""
-        aps_api_key = Config.get_value("APS_API_KEY", is_secret=True)
+        aps_api_key = Config.get_value("APS_API_KEY", is_secret=False)
         return aps_api_key
 
     @staticmethod
     def get_gov_uk_notify_api_key():
         """Retrieve the GOV.UK Notify API key from environment or secrets manager."""
-        return Config.get_value("GOV_UK_NOTIFY_API_KEY", is_secret=True)
+        return Config.get_value("GOV_UK_NOTIFY_API_KEY", is_secret=False)
 
     @staticmethod
     def get_address_verify_api_key():
         """Retrieve the address verification API key from environment or secrets manager."""
-        return Config.get_value("OS_ADDRESS_VERIFICATION_API_KEY", is_secret=True)
+        return Config.get_value("OS_ADDRESS_VERIFICATION_API_KEY", is_secret=False)
 
     @staticmethod
     def get_address_verify_client_id():
         """Retrieve the address verification Client Id from environment or secrets manager."""
-        return Config.get_value("OS_ADDRESS_VERIFICATION_CLIENT_ID", is_secret=True)
+        return Config.get_value("OS_ADDRESS_VERIFICATION_CLIENT_ID", is_secret=False)
 
     @staticmethod
     def get_address_verify_client_scope():
         """Retrieve the address verification Client Scope from environment or secrets manager."""
-        return Config.get_value("OS_ADDRESS_VERIFICATION_CLIENT_SCOPE", is_secret=True)
+        return Config.get_value("OS_ADDRESS_VERIFICATION_CLIENT_SCOPE", is_secret=False)
 
     @staticmethod
     def get_non_secret_value(key: str):
@@ -118,202 +117,140 @@ class Config:
 
     @staticmethod
     def get_gov_uk_notify_email_template_id():
-        return Config.get_value("GOV_UK_NOTIFY_EMAIL_TEMPLATE_ID", is_secret=True)
+        return Config.get_value("GOV_UK_NOTIFY_EMAIL_TEMPLATE_ID", is_secret=False)
 
     @staticmethod
     def get_gov_uk_notify_contact_us_email_template_id():
-        return Config.get_value("CONTACT_US_TEMPLATE_ID", is_secret=True)
+        return Config.get_value("CONTACT_US_TEMPLATE_ID", is_secret=False)
 
     @staticmethod
     def get_gov_uk_notify_contact_us_email_address():
-        return Config.get_value("CONTACT_US_APS_EMAIL_ADDRESS", is_secret=True)
+        return Config.get_value("CONTACT_US_APS_EMAIL_ADDRESS", is_secret=False)
 
     @staticmethod
     def get_gov_uk_notify_sms_template_id():
-        return Config.get_value("GOV_UK_NOTIFY_SMS_TEMPLATE_ID", is_secret=True)
+        return Config.get_value("GOV_UK_NOTIFY_SMS_TEMPLATE_ID", is_secret=False)
 
     @staticmethod
     def get_gov_uk_notify_api_url():
-        return Config.get_value("GOV_UK_NOTIFY_API_URL", is_secret=True)
+        return Config.get_value("GOV_UK_NOTIFY_API_URL", is_secret=False)
 
     @staticmethod
     def get_gov_uk_notify_unsubscribe_url():
-        return Config.get_value("GOV_UK_NOTIFY_UNSUBSCRIBE_URL", is_secret=True)
+        return Config.get_value("GOV_UK_NOTIFY_UNSUBSCRIBE_URL", is_secret=False)
 
     @staticmethod
     def get_aps_test_base_url():
-        return Config.get_value("APS_TEST_BASE_URL", is_secret=True)
+        return Config.get_value("APS_TEST_BASE_URL", is_secret=False)
 
     @staticmethod
     def get_azure_b2c_secret_id():
-        return Config.get_value("AZURE_B2C_CLIENT_SECRET_ID", is_secret=True)
+        return Config.get_value("AZURE_B2C_CLIENT_SECRET_ID", is_secret=False)
 
     @staticmethod
     def get_azure_b2c_client_id():
-        return Config.get_value("AZURE_B2C_CLIENT_ID", is_secret=True)
+        return Config.get_value("AZURE_B2C_CLIENT_ID", is_secret=False)
 
     @staticmethod
     def get_azure_b2c_tenant_id():
-        return Config.get_value("AZURE_B2C_TENANT_ID", is_secret=True)
+        return Config.get_value("AZURE_B2C_TENANT_ID", is_secret=False)
 
     @staticmethod
     def get_azure_b2c_tenant_name():
-        return Config.get_value("AZURE_B2C_TENANT_NAME", is_secret=True)
+        return Config.get_value("AZURE_B2C_TENANT_NAME", is_secret=False)
 
     @staticmethod
     def get_azure_b2c_policy_name():
-        return Config.get_value("AZURE_B2C_POLICY_NAME", is_secret=True)
+        return Config.get_value("AZURE_B2C_POLICY_NAME", is_secret=False)
 
     @staticmethod
     def get_azure_b2c_jwks_uri():
-        return Config.get_value("AZURE_B2C_JWKS_URI", is_secret=True)
+        return Config.get_value("AZURE_B2C_JWKS_URI", is_secret=False)
 
     @staticmethod
     def get_azure_b2c_issuer():
-        return Config.get_value("AZURE_B2C_ISSUER", is_secret=True)
+        return Config.get_value("AZURE_B2C_ISSUER", is_secret=False)
 
     @staticmethod
     def get_address_verify_base_url():
-        return Config.get_value("OS_ADDRESS_VERIFICATION_BASE_URL", is_secret=True)
+        return Config.get_value("OS_ADDRESS_VERIFICATION_BASE_URL", is_secret=False)
 
     @staticmethod
     def get_address_verify_token_url():
-        return Config.get_value("OS_ADDRESS_VERIFICATION_TOKEN_URL", is_secret=True)
+        return Config.get_value("OS_ADDRESS_VERIFICATION_TOKEN_URL", is_secret=False)
 
     @staticmethod
     def get_hpub_base_api_url():
-        return Config.get_value("HPUB_FRONTEND_URL", is_secret=True)
+        return Config.get_value("HPUB_FRONTEND_URL", is_secret=False)
 
     @staticmethod
     def get_hpub_event_bridge_source():
-        return Config.get_value("HPUB_EVENT_BRIDGE_SOURCE", is_secret=True)
+        return Config.get_value("HPUB_EVENT_BRIDGE_SOURCE", is_secret=False)
 
     @staticmethod
     def get_hpub_event_bridge_bus_name():
-        return Config.get_value("HPUB_EVENT_BRIDGE_BUS_NAME", is_secret=True)
+        return Config.get_value("HPUB_EVENT_BRIDGE_BUS_NAME", is_secret=False)
 
     @staticmethod
     def get_hpub_event_bridge_detail_type_order_creation():
         return Config.get_value(
-            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_ORDER_CREATION", is_secret=True
+            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_ORDER_CREATION", is_secret=False
         )
 
     @staticmethod
     def get_hpub_event_bridge_detail_type_product_draft():
         return Config.get_value(
-            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_PRODUCT_DRAFT", is_secret=True
+            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_PRODUCT_DRAFT", is_secret=False
         )
 
     @staticmethod
     def get_hpub_event_bridge_detail_type_product_archive():
         return Config.get_value(
-            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_PRODUCT_ARCHIVE", is_secret=True
+            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_PRODUCT_ARCHIVE", is_secret=False
         )
 
     @staticmethod
     def get_hpub_event_bridge_detail_type_product_withdrawn():
         return Config.get_value(
-            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_PRODUCT_WITHDRAWN", is_secret=True
+            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_PRODUCT_WITHDRAWN", is_secret=False
         )
 
     @staticmethod
     def get_hpub_event_bridge_detail_type_product_live():
         return Config.get_value(
-            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_PRODUCT_LIVE", is_secret=True
+            "HPUB_EVENT_BRIDGE_DETAIL_TYPE_PRODUCT_LIVE", is_secret=False
         )
 
     @staticmethod
     def get_django_secret_key():
-        return Config.get_value("DJANGO_SECRET_KEY", is_secret=True)
-
-    @staticmethod
-    def get_postgres_connection_regex():
-        """Retrieve PostgreSQL connection regex from secrets manager."""
-        pattern = Config.get_value("POSTGRES_CONNECTION_REGEX", is_secret=True)
-        if not pattern:
-            raise ValueError("POSTGRES_CONNECTION_REGEX is not defined or empty.")
-        return pattern
+        return Config.get_value("DJANGO_SECRET_KEY", is_secret=False)
 
     @staticmethod
     def get_rsa_private_key():
         """Retrieve the RSA private key from AWS Secrets Manager."""
-        # Retrieve the secret ID from your configuration (from .env.dev)
-        secret_id = Config.get_non_secret_value("RSA_KEYS_SECRET_ID")
-        if not secret_id:
-            logger.error(RSA_KEYS_SECRET_ID_ERROR_MSG)
-            raise EnvironmentError(RSA_KEYS_SECRET_ID_ERROR_MSG)
-        secret_data = get_secret_value(secret_id)
-
-        # Parse the JSON to extract the private key value
-        private_key_data = json.loads(secret_data)
-        return private_key_data["RSA_PRIVATE_KEY"]
+        return Config.get_value("RSA_PRIVATE_KEY", is_secret=False)
 
     @staticmethod
     def get_rsa_public_key():
         """Retrieve the RSA public key from AWS Secrets Manager."""
-        # Retrieve the secret ID from your configuration (from .env.dev)
-        secret_id = Config.get_non_secret_value("RSA_KEYS_SECRET_ID")
-        if not secret_id:
-            logger.error(RSA_KEYS_SECRET_ID_ERROR_MSG)
-            raise EnvironmentError(RSA_KEYS_SECRET_ID_ERROR_MSG)
-        secret_data = get_secret_value(secret_id)
-        # Parse the JSON to extract the public key value
-        public_key_data = json.loads(secret_data)
-        return public_key_data["RSA_PUBLIC_KEY"]
+        return Config.get_value("RSA_PUBLIC_KEY", is_secret=False)
 
     @staticmethod
-    def parse_connection_string(connection_string: str):
-        """Parse PostgreSQL connection string and extract components."""
-        try:
-            # Fetch the regex pattern
-            pattern = Config.get_postgres_connection_regex()
+    def get_db_port():
+        return Config.get_value("DB_PORT", is_secret=False)
 
-            # Use the pattern in `re.match`
-            match = re.match(pattern, connection_string)
-            if not match:
-                logger.error(f"Invalid connection string format: {connection_string}")
-                raise ValueError(
-                    f"Invalid connection string format: {connection_string}"
-                )
-            return match.groupdict()
-        except Exception as e:
-            logger.error(f"Error parsing connection string: {e}")
-            raise
+    @staticmethod
+    def get_db_user():
+        return Config.get_value("DB_USER", is_secret=False)
 
-    @property
-    def db_connection_details(self):
-        """
-        Retrieve and parse the PostgreSQL connection string (stored as a secret).
-        Storing the entire connection string as a single secret simplifies management and retrieval.
-        Avoids the complexity of handling multiple secrets for each component.
-        """
-        connection_string = self.get_value(
-            "HPUB_POSTGRES_CONNECTION_STRING", is_secret=True
-        )
+    @staticmethod
+    def get_db_password():
+        return Config.get_value("DB_PASSWORD", is_secret=False)
 
-        # Parse the connection string directly since it's a plain string
-        try:
-            return self.parse_connection_string(connection_string)
-        except ValueError as e:
-            logger.error(f"Error extracting connection string: {e}")
-            raise ValueError("Invalid format for connection string data")
+    @staticmethod
+    def get_db_host():
+        return Config.get_value("DB_HOST", is_secret=False)
 
-    @property
-    def DB_NAME(self):
-        return self.db_connection_details["dbname"]
-
-    @property
-    def DB_USER(self):
-        return self.db_connection_details["user"]
-
-    @property
-    def DB_PASSWORD(self):
-        return self.db_connection_details["password"]
-
-    @property
-    def DB_HOST(self):
-        return self.db_connection_details["host"]
-
-    @property
-    def DB_PORT(self):
-        return self.db_connection_details["port"]
+    @staticmethod
+    def get_db_name():
+        return Config.get_value("DB_NAME", is_secret=False)

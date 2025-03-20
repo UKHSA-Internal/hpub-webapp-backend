bcf87e7d85e67566293db9c501b54f7190e4b773
ebubeoguchi34 <ebube.oguchi@ukhsa.gov.uk>
2025-03-20 14:26:10 +0000

    refactor feedback model and serializer to include additional fields for enhanced user feedback collection and update viewset for improved user reference handling

--- health_pubs/core/feedbacks/serializers.py
+++ health_pubs/core/feedbacks/serializers.py
@@ -1,18 +1,25 @@
 import uuid
-
 from rest_framework import serializers
-
 from .models import Feedback
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
-        fields = ["feedback_id", "user_ref", "message", "submitted_at"]
+        fields = [
+            "feedback_id",
+            "user_ref",
+            "how_satisfied",
+            "would_recommend",
+            "where_did_you_hear",
+            "why_did_you_come",
+            "did_you_get_what_you_wanted",
+            "improve_our_service",
+            "submitted_at",
+        ]
 
     def create(self, validated_data):
-        # Check if the 'feedback_id' is provided in the request
-        feedback_id = validated_data.get("feedback_id", None)
-        if not feedback_id:
+        # If feedback_id wasn't provided, auto-generate
+        if not validated_data.get("feedback_id"):
             validated_data["feedback_id"] = uuid.uuid4()
         return super().create(validated_data)

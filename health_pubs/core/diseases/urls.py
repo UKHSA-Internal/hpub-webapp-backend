2a10a6b5ee766e5bd38c58ad3be816b2c272df03
ebubeoguchi34 <ebube.oguchi@ukhsa.gov.uk>
2025-03-24 15:11:22 +0000

    Add DiseaseDeleteViewSet for individual disease deletion

--- health_pubs/core/diseases/urls.py
+++ health_pubs/core/diseases/urls.py
@@ -1,10 +1,16 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import DiseaseCreateViewSet, DiseaseDeleteAllViewSet, DiseaseListViewSet
+from .views import (
+    DiseaseCreateViewSet,
+    DiseaseDeleteAllViewSet,
+    DiseaseDeleteViewSet,
+    DiseaseListViewSet,
+)
 
 router = DefaultRouter()
 router.register(r"diseases/create", DiseaseCreateViewSet, basename="disease-create")
 router.register(r"diseases", DiseaseListViewSet, basename="disease-list")
+router.register(r"diseases/delete", DiseaseDeleteViewSet, basename="disease-delete")
 router.register(
     r"diseases/delete-all", DiseaseDeleteAllViewSet, basename="disease-delete-all"
 )

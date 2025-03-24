e5e510492aab826c945cc7cd0ac62350d21a6d33
ebubeoguchi34 <ebube.oguchi@ukhsa.gov.uk>
2025-03-24 16:06:03 +0000

    Refactor disease deletion logic and restore DiseaseListViewSet to filter live diseases

--- health_pubs/core/diseases/urls.py
+++ health_pubs/core/diseases/urls.py
@@ -2,7 +2,6 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (
     DiseaseCreateViewSet,
-    DiseaseDeleteAllViewSet,
     DiseaseDeleteViewSet,
     DiseaseListViewSet,
 )
@@ -11,8 +10,6 @@ router = DefaultRouter()
 router.register(r"diseases/create", DiseaseCreateViewSet, basename="disease-create")
 router.register(r"diseases", DiseaseListViewSet, basename="disease-list")
 router.register(r"diseases/delete", DiseaseDeleteViewSet, basename="disease-delete")
-router.register(
-    r"diseases/delete-all", DiseaseDeleteAllViewSet, basename="disease-delete-all"
-)
+
 
 urlpatterns = router.urls

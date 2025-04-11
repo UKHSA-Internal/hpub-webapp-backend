35ead60dde442b7da59a9dcd68fcccfeb04ee559
shashikiran2022 <shashikiran.subramanya@ukhsa.gov.uk>
2025-04-11 17:29:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,13 +17,13 @@ migrations_output=$(python manage.py showmigrations --verbosity=2 --no-color 2>&
   exit 1
 }
 echo "$migrations_output"
-echo "DB USER: $DB_USER"
-
 # -----------------------------------------------------------------------------
 # Step 3: Count pending migrations
 # -----------------------------------------------------------------------------
 # Remove any ANSI color codes (just in case)
+echo "clean"
 clean_output=$(echo "$migrations_output" | sed 's/\x1B\[[0-9;]*[a-zA-Z]//g')
+echo "$clean_output"
 # Count lines that have the pending migration marker, assuming lines start with optional whitespace then "[ ]"
 pending_count=$(echo "$clean_output" | grep -E -c "^\s*\[ \]")
 echo "Number of pending migrations: $pending_count"

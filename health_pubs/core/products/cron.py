86d9d7825490c9ed173837c72ed78f68d0de4bb9
ebubeoguchi34 <ebube.oguchi@ukhsa.gov.uk>
2025-04-17 12:12:43 +0100

    Update Dockerfile and entrypoint to set timezone and schedule cron job at 7AM

--- health_pubs/core/products/cron.py
+++ health_pubs/core/products/cron.py
@@ -1,6 +1,6 @@
 CRONJOBS = [
     (
-        "0 0 * * *",
+        "0 7 * * *",
         "products.cron.CheckDraftProductsCronJob.do",
-    )  # Runs at midnight every day
+    )  # Runs at 7 AM Europe/London each day
 ]

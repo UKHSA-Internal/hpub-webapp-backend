6451cfa63200e920abeee131a007c2148a2a0d68
ebubeoguchi34 <ebube.oguchi@ukhsa.gov.uk>
2025-05-24 00:36:40 +0000

    Adding fix for pre registeration api

--- health_pubs/core/users/urls.py
+++ health_pubs/core/users/urls.py
@@ -9,9 +9,11 @@ from .views import (
     UserListView,
     UserLoginView,
     UserSignUpView,
+    pre_registration,
 )
 
 urlpatterns = [
+    path("users/pre-registration/", pre_registration, name="b2c-pre-registration"),
     path("users/signup/", UserSignUpView.as_view(), name="signup"),
     path("users/update/", UpdateUserView.as_view(), name="update-user-view"),
     path("users/login/", UserLoginView.as_view(), name="login"),

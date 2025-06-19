a8c9e339632f5bc81c32aadd7b6172218e36957e
ebubeoguchi34 <ebube.oguchi@ukhsa.gov.uk>
2025-06-19 19:12:00 +0100

    feat: add AuthStatusView to return current authentication status and user metadata

--- health_pubs/core/users/urls.py
+++ health_pubs/core/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import (
+    AuthStatusView,
     LogoutView,
     MigrateUsersAPIView,
     TokenRefresh,
@@ -21,6 +22,7 @@ urlpatterns = [
     path("users/signup/", UserSignUpView.as_view(), name="signup"),
     path("users/update/", UpdateUserView.as_view(), name="update-user-view"),
     path("users/login/", UserLoginView.as_view(), name="login"),
+    path("users/auth/status/", AuthStatusView.as_view(), name="auth-status"),
     path("users/logout/", LogoutView().as_view(), name="logout"),
     path("users/refresh/", TokenRefresh.as_view(), name="token_refresh"),
     path("users/<uuid:user_id>/", UserDetailView.as_view(), name="user-detail"),

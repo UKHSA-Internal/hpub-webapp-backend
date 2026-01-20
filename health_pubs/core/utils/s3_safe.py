ae4ae37719066dab4814d84020c59ccb3b4068a4
terry-ukhsa <terry.phung@ukhsa.gov.uk>
2026-01-20 16:59:26 +0000

    fix: fix s3 call leading erroring due to extra ExpectedBucketOwner arg in temp file download, and fix missing . for .pdf causing method to not load metadata

--- health_pubs/core/utils/s3_safe.py
+++ health_pubs/core/utils/s3_safe.py
@@ -5,8 +5,10 @@ from django.conf import settings
 s3_client = boto3.client("s3")
 S3_EXPECTED_OWNER = getattr(settings, "AWS_EXPECTED_BUCKET_OWNER", None)
 
+#ExpectedBucketOwner isnt supported by all methods, leave it out
+UNSUPPORTED_METHODS = {"download_fileobj"}
 
 def s3_call(method: str, **kwargs):
-    if S3_EXPECTED_OWNER:
+    if S3_EXPECTED_OWNER and method not in UNSUPPORTED_METHODS:
         kwargs.setdefault("ExpectedBucketOwner", S3_EXPECTED_OWNER)
     return getattr(s3_client, method)(**kwargs)

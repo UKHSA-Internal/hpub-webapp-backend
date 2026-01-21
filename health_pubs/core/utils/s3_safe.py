c11dcbb1ba5c9f27220d04b6bc9f9fd3200eae59
Terry <terry.phung@ukhsa.gov.uk>
2026-01-21 15:37:01 +0000

    Update health_pubs/core/utils/s3_safe.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- health_pubs/core/utils/s3_safe.py
+++ health_pubs/core/utils/s3_safe.py
@@ -8,6 +8,7 @@ S3_EXPECTED_OWNER = getattr(settings, "AWS_EXPECTED_BUCKET_OWNER", None)
 #ExpectedBucketOwner isnt supported by all methods, leave it out
 UNSUPPORTED_METHODS = {"download_fileobj"}
 
+
 def s3_call(method: str, **kwargs):
     if S3_EXPECTED_OWNER and method not in UNSUPPORTED_METHODS:
         kwargs.setdefault("ExpectedBucketOwner", S3_EXPECTED_OWNER)

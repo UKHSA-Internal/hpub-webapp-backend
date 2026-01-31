eea0a7322478e34c3bdbc12c97e244d96a0c2832
Vikash Kothary <vikash.kothary@ukhsa.gov.uk>
2026-01-31 02:03:54 +0000

    refactor: Move views into a package

---
+++ health_pubs/core/users/views/__init__.py
@@ -0,0 +1,12 @@
+from .views import (
+    AuthStatusView,
+    LogoutView,
+    MigrateUsersAPIView,
+    TokenRefresh,
+    UpdateUserView,
+    UserDetailView,
+    UserListView,
+    UserLoginView,
+    UserSignUpView,
+    PreRegistrationView,
+)

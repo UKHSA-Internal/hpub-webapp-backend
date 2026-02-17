9931682d766ab0080cabb046ca2e713f882fd1ef
xuwangukhsa <Xu.Wang@ukhsa.gov.uk>
2026-02-17 15:52:53 +0000

    Apply suggestion from @github-actions[bot]
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- health_pubs/core/users/urls.py
+++ health_pubs/core/users/urls.py
@@ -26,7 +26,11 @@ urlpatterns = [
     path("users/logout/", LogoutView().as_view(), name="logout"),
     path("users/refresh/", TokenRefresh.as_view(), name="token_refresh"),
     path("users/<uuid:user_id>/", UserDetailView.as_view(), name="user-detail"),
-    path("users/string-id/<str:user_id>/", UserDetailView.as_view(),name="user-detail-by-string-id"),
+    path(
+        "users/string-id/<str:user_id>/",
+        UserDetailView.as_view(),
+        name="user-detail-by-string-id",
+    ),
     path("users/list/", UserListView.as_view(), name="user-list"),
     path("users/migrate-users/", MigrateUsersAPIView.as_view(), name="user-migrate"),
 ]

a1c81e92f8faf5f02811de333e179f98d5e94415
Vikash Kothary <vikash.kothary@ukhsa.gov.uk>
2026-03-23 21:46:37 +0000

    feat: [HPUB-2750] Add auth endpoints

--- health_pubs/core/auth/urls.py
+++ health_pubs/core/auth/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import get_access_token
+from . import views
 
 urlpatterns = [
-    path('api/v2/auth/token/', get_access_token, name='auth'),
+    path("api/v2/auth/decode/", views.get_token),
+    path("api/v2/auth/users/", views.get_user),
+    path("api/v2/auth/token/", views.create_access_token),
+    path("api/v2/auth/refresh/", views.refresh_access_token),
+    path("api/v2/auth/revoke/", views.revoke_access_token),
 ]

48e95f60cbb2bf3049ef2576ab26a605a97bf94b
Vikash Kothary <vikash.kothary@ukhsa.gov.uk>
2026-03-28 18:30:28 +0000

    feat: [HPUB-2750] Add placeholder endpoints for roles v2

--- health_pubs/core/roles/urls.py
+++ health_pubs/core/roles/urls.py
@@ -2,9 +2,23 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from core.roles.views import v1
+from core.roles.views import v2
 
 router = DefaultRouter()
-router.register("api/v1/roles", v1.RoleViewSet)
+
+# v1 roles
+router.register(
+    r"api/v1/roles",
+    v1.RoleViewSet,
+    basename="roles-v1"
+)
+
+# v2 roles
+router.register(
+    r"api/v2/roles",
+    v2.RolesV2,
+    basename="roles-v2"
+)
 
 urlpatterns = [
     path("", include(router.urls)),
---
+++ health_pubs/core/roles/views/v2.py
@@ -0,0 +1,73 @@
+from rest_framework import viewsets
+from rest_framework import filters
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework import authentication
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+from core.roles.serializers import RoleSerializer
+from core.roles.models import Role
+from core.utils import logging_utils
+
+
+logger = logging_utils.get_logger(__name__)
+
+class CustomPagination(PageNumberPagination):
+    from django.conf import settings
+
+    page_size = getattr(
+        settings, "USERS_LIST_PAGE_SIZE", 10
+    )  # Set pagination to 10 items per page
+
+    def get_paginated_response(self, data, status_code=200):
+        response = Response(
+            {
+                "metadata": {
+                    "total_count": self.page.paginator.count,
+                    "page_size": self.page_size,
+                    "page_number": self.page.number,
+                    "next_page": self.get_next_link(),
+                    "previous_page": self.get_previous_link(),
+                },
+                "data": data,
+            },
+            status=status_code,
+        )
+        return response
+
+
+class RolesV2(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = [
+        "name"
+    ]
+    search_fields = [
+        "name"
+    ]
+    pagination_class = CustomPagination
+
+
+    def list(self, request):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def retrieve(self, request, pk=None):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def create(self, request):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def update(self, request, pk=None):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def partial_update(self, request, pk=None):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def destroy(self, request, pk=None):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)

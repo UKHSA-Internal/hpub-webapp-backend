781c277fed86abf4513306740e65450f6787a4b9
Vikash Kothary <vikash.kothary@ukhsa.gov.uk>
2026-03-28 19:40:31 +0000

    refactor: [HPUB-2750] Create common view set for placeholder endpoints

---
+++ health_pubs/core/common/views.py
@@ -0,0 +1,45 @@
+from rest_framework import viewsets
+from rest_framework import filters
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework import authentication
+from rest_framework.response import Response
+
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+from core.common.serializers import ListResponse
+from core.utils import logging_utils
+
+
+logger = logging_utils.get_logger(__name__)
+
+
+class CommonViewSet(viewsets.ModelViewSet):
+    queryset = None
+    serializer_class = None
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = []
+    search_fields = []
+    pagination_class = ListResponse
+
+
+    def list(self, request):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def retrieve(self, request, pk=None):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def create(self, request):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def update(self, request, pk=None):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def partial_update(self, request, pk=None):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)
+
+    def destroy(self, request, pk=None):
+        return Response({"detail": "Not implemented"}, status=status.HTTP_501_NOT_IMPLEMENTED)

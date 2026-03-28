138d3a5b34cb118ebf28fd2eea1a6f325bcb7cef
Vikash Kothary <vikash.kothary@ukhsa.gov.uk>
2026-03-28 18:09:53 +0000

    refactor: [HPUB-2750] Separate roles v1 endpoints

--- health_pubs/core/roles/urls.py
+++ health_pubs/core/roles/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import RoleViewSet
+from core.roles.views import v1
 
 router = DefaultRouter()
-router.register(r"roles", RoleViewSet)
+router.register("api/v1/roles", v1.RoleViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),

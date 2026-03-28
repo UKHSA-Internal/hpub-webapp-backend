9ceb8fc8f4d48466b0580a62bfcd31dd3e16fed8
Vikash Kothary <vikash.kothary@ukhsa.gov.uk>
2026-03-28 21:09:07 +0000

    feat: [HPUB-2750] Add placeholder addresses v2 endpoints

--- health_pubs/core/addresses/urls.py
+++ health_pubs/core/addresses/urls.py
@@ -2,9 +2,19 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from core.addresses.views import v1
+from core.addresses.views import v2
 
 router = DefaultRouter()
-router.register(r"api/v1/addresses",v1.AddressViewSet)
+router.register(
+    r"api/v1/addresses",
+    v1.AddressViewSet,
+    basename='addresses-v1'
+)
+router.register(
+    r"api/v2/users/(?P<user_id>[^/.]+)/addresses",
+    v2.UserAddressesV2,
+    basename='addresses-v2'
+)
 
 urlpatterns = [
     path("", include(router.urls)),
---
+++ health_pubs/core/addresses/views/v2.py
@@ -0,0 +1,58 @@
+from rest_framework import status, permissions, authentication, viewsets
+from rest_framework.response import Response
+
+from core.utils import logging_utils
+from core.users.models import User 
+from core.addresses.serializers import AddressSerializer
+from core.addresses.models import Address
+
+
+logger = logging_utils.get_logger(__name__)
+
+
+class UserAddressesV2(viewsets.ModelViewSet):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.AllowAny]
+    serializer_class = AddressSerializer
+
+
+    def get_queryset(self):
+        user_id = self.kwargs.get("user_id")
+        return User.objects.filter(user_id=user_id)
+
+
+    def list(self, request, user_id=None):
+        return Response(
+            {"detail": "Not implemented"},
+            status=status.HTTP_501_NOT_IMPLEMENTED
+        )
+
+    def retrieve(self, request, user_id=None, pk=None):
+        return Response(
+            {"detail": "Not implemented"},
+            status=status.HTTP_501_NOT_IMPLEMENTED
+        )
+
+    def create(self, request, user_id=None):
+        return Response(
+            {"detail": "Not implemented"},
+            status=status.HTTP_501_NOT_IMPLEMENTED
+        )
+
+    def update(self, request, user_id=None, pk=None):
+        return Response(
+            {"detail": "Not implemented"},
+            status=status.HTTP_501_NOT_IMPLEMENTED
+        )
+
+    def partial_update(self, request, user_id=None, pk=None):
+        return Response(
+            {"detail": "Not implemented"},
+            status=status.HTTP_501_NOT_IMPLEMENTED
+        )
+
+    def destroy(self, request, user_id=None, pk=None):
+        return Response(
+            {"detail": "Not implemented"},
+            status=status.HTTP_501_NOT_IMPLEMENTED
+        )

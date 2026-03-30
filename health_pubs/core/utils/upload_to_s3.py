e229393f977fc5e465b2393be55195e7582c5e48
KajananUKHSA <Kajanan.Lingkeswaran@ukhsa.gov.uk>
2026-03-30 12:01:06 +0100

    refactor: removed redunant script execution containing pii

--- health_pubs/core/utils/upload_to_s3.py
+++ health_pubs/core/utils/upload_to_s3.py
@@ -37,17 +37,3 @@ def upload_file_to_s3(file_name: str, bucket: str, object_name: str = None):
 
     logger.info(f"File {file_name} uploaded to {bucket}/{object_name}.")
     return True
-
-
-# Example usage
-if __name__ == "__main__":
-    # Replace these with your own values
-    # Path to the file you want to upload
-    file_name = "/home/ebubeoguchi/hpub-webapp/hpub-backend/health_pubs/core/utils/audio_transcript.txt"
-    bucket_name = "hpub-publications-media-dev"  # Your S3 bucket name
-    object_name = (
-        "audio_transcript.txt"  # The name that will be used for the file in S3
-    )
-
-    # Call the function to upload the file
-    upload_file_to_s3(file_name, bucket_name, object_name)

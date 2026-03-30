7ef6cfc334d70a6a2b78cf0848898f5f50bff1a6
Vikash Kothary <vikash.kothary@ukhsa.gov.uk>
2026-03-30 14:24:59 +0100

    feat: Make all v2 endpoints admin only

--- health_pubs/core/auth/views.py
+++ health_pubs/core/auth/views.py
@@ -5,48 +5,49 @@ from rest_framework.decorators import (
     permission_classes,
     authentication_classes,
 )
-from rest_framework.permissions import AllowAny
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
 
 from core.auth import services
+from core.utils import custom_token_authentication
 
 
 @api_view(["GET"])
-@authentication_classes([])
-@permission_classes([AllowAny])
+@authentication_classes([custom_token_authentication.CustomTokenAuthentication])
+@permission_classes([permissions.IsAuthenticated, permissions.IsAdminUser])
 def get_token(request):
     data = services.decode_access_token(request)
     return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(["GET"])
-@authentication_classes([])
-@permission_classes([AllowAny])
+@authentication_classes([custom_token_authentication.CustomTokenAuthentication])
+@permission_classes([permissions.IsAuthenticated, permissions.IsAdminUser])
 def get_user(request):
     data = services.get_user_from_access_token(request)
     return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
-@authentication_classes([])
-@permission_classes([AllowAny])
+@authentication_classes([custom_token_authentication.CustomTokenAuthentication])
+@permission_classes([permissions.IsAuthenticated, permissions.IsAdminUser])
 def create_access_token(request):
     data = services.create_access_token(request)
     return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
-@authentication_classes([])
-@permission_classes([AllowAny])
+@authentication_classes([custom_token_authentication.CustomTokenAuthentication])
+@permission_classes([permissions.IsAuthenticated, permissions.IsAdminUser])
 def refresh_access_token(request):
     data = services.refresh_access_token()
     return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
-@authentication_classes([])
-@permission_classes([AllowAny])
+@authentication_classes([custom_token_authentication.CustomTokenAuthentication])
+@permission_classes([permissions.IsAuthenticated, permissions.IsAdminUser])
 def revoke_access_token(request):
     data = services.revoke_access_token(request)
     return Response(data, status=status.HTTP_200_OK)
